11e078f267d93806dd4b7db696652a882e39b670
Aratar2 <aratar5@yandex.ru>
2025-10-12 17:00:42 +0200

    Ensure nginx TLS snippets are available

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -10,10 +10,24 @@ fi
 
 TEMPLATE_DIR="/etc/nginx/templates"
 CONF_PATH="/etc/nginx/conf.d/default.conf"
-CERT_BASE="/etc/letsencrypt/live"
+CERT_ROOT="/etc/letsencrypt"
+CERT_BASE="${CERT_ROOT}/live"
 CERT_DIR="${CERT_BASE}/${DOMAIN}"
+SNIPPET_SOURCE_DIR="/etc/nginx/ssl"
 
 mkdir -p "$CERT_BASE"
+
+ensure_tls_snippets() {
+  mkdir -p "$CERT_ROOT"
+
+  if [ -f "${SNIPPET_SOURCE_DIR}/options-ssl-nginx.conf" ] && [ ! -f "${CERT_ROOT}/options-ssl-nginx.conf" ]; then
+    install -m 0644 "${SNIPPET_SOURCE_DIR}/options-ssl-nginx.conf" "${CERT_ROOT}/options-ssl-nginx.conf"
+  fi
+
+  if [ -f "${SNIPPET_SOURCE_DIR}/ssl-dhparams.pem" ] && [ ! -f "${CERT_ROOT}/ssl-dhparams.pem" ]; then
+    install -m 0644 "${SNIPPET_SOURCE_DIR}/ssl-dhparams.pem" "${CERT_ROOT}/ssl-dhparams.pem"
+  fi
+}
 
 render_config() {
   if [ -f "${CERT_DIR}/fullchain.pem" ] && [ -f "${CERT_DIR}/privkey.pem" ]; then
@@ -63,6 +77,8 @@ watch_certificates() {
     fi
   done
 }
+
+ensure_tls_snippets
 
 render_config
 
